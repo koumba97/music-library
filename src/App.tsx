22c7a9c8c72b1e7113e594f192077bf4ea5f3c4c
Koumba <koumba.keita@vadesecure.com>
2023-05-02 14:27:28 +0200

    Connect react-app to firebase to fetch the album list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
 import  React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getDocs, collection } from "firebase/firestore"; 
+import {db} from './firebase_setup/firebase'
 
 interface IProps {
 }
 
 interface IState {
+    albums: IAlbum[];
+}
+
+interface IAlbum {
+    artist: string;
+    title: string;
+    id: string;
+    year: number;
+    image: string;
 }
 
 class App extends React.Component<IProps, IState> {
+    
     constructor(props: IProps) {
         super(props);
-
+        
         this.state = {
+            albums: [],
         }
     }
 
-    render () {
+    componentDidMount(): void {
+        this.getAlbums();
+    }
+    submithandler(e: any) {
+      e.preventDefault()
+    }
+
+    getAlbums = async () => {
+        const doc_refs = await getDocs(collection(db, 'albums')).then((result)=>{               
+            const albums = result.docs.map((doc) => ({...doc.data(), id:doc.id })) as IAlbum[];              
+    
+            this.setState({
+                albums: albums
+            });
+        })
+    }
+    
+
+    render () {  
         return (
             <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                </header>
+                {this.state.albums.map((album, index) => {
+                    return (
+                        <h1 key={index}>{album.title}</h1>
+                    );
+                })}
             </div>
         )
     }
