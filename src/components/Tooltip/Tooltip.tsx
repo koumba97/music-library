d7e9bedd54b629bad9b9f9383205693bc912a62b
Koumba <koumba.keita@vadesecure.com>
2023-05-15 17:46:35 +0200

    Convert Tooltip class component -> to functional component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,20 +1,13 @@
-import React from "react";
 import './Tooltip.scss';
 
 interface IProps {
     content: string;
 }
 
-export default class Tooltip extends React.Component<IProps>{
-    constructor(props: IProps) {
-        super(props);
-    }
-
-    render() {
-        const { props } = this;
-
-        return(
-            <div className="tooltip-text">{props.content}</div>
-        )
-    }
+const Tooltip = (props: IProps) => {
+    return (
+        <div className="tooltip-text">{props.content}</div>
+    );
 }
+
+export default Tooltip;
