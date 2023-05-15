be25ae94f03018b2f2b5c2d64926ff59751011e6
Koumba <koumba.keita@vadesecure.com>
2023-05-15 17:58:54 +0200

    Convert CardList class component -> to functional component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,43 +1,30 @@
-import React from "react";
 import './CardList.scss';
 import AlbumCard from "../AlbumCard/AlbumCard";
 import { IAlbum } from "../../App";
 
-
 type Item = any;
 
- interface IItem extends Item {
+interface IItem extends Item {
     id : string | number;
     title: string;
- }
+}
 
 interface IProps {
     list: IItem[];
     listType: string;
 }
 
-interface IState {
+const CardList = (props: IProps) => {
+    return (
+        <div className="card-list">
+            {props.list.map(item => {
+                if(props.listType === 'album')
+                return (
+                    <AlbumCard key={item.id} album={item as IAlbum}></AlbumCard>
+                )
+            })}
+        </div>
+    );
+};
 
-}
-
-export default class CardList extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-    }
-
-    render() {
-        const { props } = this;
-
-        if(props.list)
-        return (
-            <div className="card-list">
-                {props.list.map(item => {
-                    if(props.listType === 'album')
-                    return (
-                        <AlbumCard key={item.id} album={item as IAlbum}></AlbumCard>
-                    )
-                })}
-            </div>
-        )
-    }
-}
+export default CardList;
